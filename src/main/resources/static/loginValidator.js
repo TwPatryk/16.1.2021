e308f65c71de4f5d8c5deb6b8c998561565d9bd3
TwPatryk <72356940+TwPatryk@users.noreply.github.com>
2021-02-06 17:43:26 +0100

    15.11  03:46

--- src/main/resources/static/loginValidator.js
+++ src/main/resources/static/loginValidator.js
@@ -2,17 +2,20 @@ function validate() {
     var login = document.forms["loginForm"]["login"].value;
     var pass = document.forms["loginForm"]["pass"].value;
 
+    var regex = new RegExp(/.{3}.*/);
+
     var flag = true;
 
-    if(login == "") {
+    if(!regex.test(login)) {
         document.forms["loginForm"]["login"].style.background = "#eec4c4";
         flag = false;
     }
 
-    if(pass == "") {
+    if(!regex.test(pass)) {
         document.forms["loginForm"]["pass"].style.background = "#eec4c4";
         flag = false;
     }
 
     return flag;
 }
+
